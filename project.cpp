c29080af24a8d22c31cbd59dc7eb863e46d4c9c4
shams018 <nasirygo1122@gmail.com>
2025-05-27 11:06:37 +0500

    Add files via upload
    Added the string arrays for moths and days to use them in functions also added the iomanip header file to use its operations finnally the printMonthCalender was added which uses other previsouly added functions to display the on demand weekday and month of the user

--- project.cpp
+++ project.cpp
@@ -1,5 +1,11 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
+
+// Month and weekday names
+string months[12] = { "January", "February", "March", "April", "May", "June",
+                      "July", "August", "September", "October", "November", "December" };
+string days[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 // Check leap year
 bool isLeapYear(int year) {
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
@@ -23,6 +29,24 @@ int getWeekday(int day, int month, int year) {
     int K = year % 100, J = year / 100;
     int h = (day + 13 * (month + 1) / 5 + K + K/4 + J/4 + 5 * J) % 7;
     return (h + 6) % 7;
+}
+// Print a calendar 
+void printMonthCalendar(int month, int year) {
+    cout << "     " << months[month - 1] << " " << year << endl;
+    cout << "Su Mo Tu We Th Fr Sa" << endl;
+
+    int startDay = getWeekday(1, month, year);
+    int numDays = getDaysInMonth(month, year);
+
+    for (int i = 0; i < startDay; ++i)
+        cout << "   ";
+
+    for (int day = 1; day <= numDays; ++day) {
+        cout << setw(2) << day << " ";
+        if ((startDay + day) % 7 == 0)
+            cout << endl;
+    }
+    cout << "\n" << endl;
 }
 
 int main(){
